eccaa2ce9aed14953d6330ab6b546f600e04ae5e
Nitish <rananitish37@gmail.com>
2023-04-25 17:53:27 +0530

    midify intro.java

--- ArrayList/intro.java
+++ ArrayList/intro.java
@@ -1,6 +1,7 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class intro {
     public static void main(String[] args) {
@@ -34,5 +35,32 @@ public class intro {
         list.add(1, 2);
         System.out.println(list);
         
+        //size()
+        System.out.println("size= "+list.size());
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+        list.add(5);
+        System.out.println("before swap: "+list);
+        //swap in Arralist
+        int idx1=1,idx2=3;
+        swap(list,idx1,idx2);
+        System.out.println("after swap: "+list);
+
+        //Collextion.sort()
+        System.out.println("Collections.sort()======");
+        System.out.println("before sort: "+list);
+        Collections.sort(list);
+        System.out.println("After sort: "+list);
+
+        //Collection.sort(list,order)
+        System.out.println("sort in reverse order======");
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println("list in reverse: "+list);
+    }
+    public static void swap(ArrayList<Integer> list,int idx1,int idx2){
+        int temp=list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
     }
 }
