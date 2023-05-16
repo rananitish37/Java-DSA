fa458298994b3c468948ffa5756106dc2c75c415
Nitish <rananitish37@gmail.com>
2023-05-17 00:52:22 +0530

    .\kLargestUsingPQ.java added

---
+++ Queue/kLargestUsingPQ.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class kLargestUsingPQ {
+    static int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            if(i<k){
+                pq.add(arr[i]);
+            }else{
+                if(pq.peek() < arr[i]){
+                    pq.poll();
+                    pq.add(arr[i]);
+                }
+            }
+        }
+        ArrayList<Integer> lst=new ArrayList<>(pq);
+        Collections.sort(lst,Collections.reverseOrder());
+        System.out.println(lst.size());
+        int [] res = new int[lst.size()];
+        for(int i=0;i<lst.size();i++){
+            res[i]=lst.get(i);
+        }
+        System.out.println(lst);
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[]={12,5,787,1,23};
+        int res[]=kLargest(arr, 5, 2);
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+}
