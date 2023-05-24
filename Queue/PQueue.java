8ea97d36c881b38158f1f24b1312107cb867addc
Nitish <rananitish37@gmail.com>
2023-05-25 00:51:17 +0530

    .\PQueue.java added

---
+++ Queue/PQueue.java
@@ -0,0 +1,10 @@
+package Queue;
+import java.util.*;
+public class PQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.add(1);
+        pq.add(2);
+        System.out.println(pq);
+    }
+}
