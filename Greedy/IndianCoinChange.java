501fec031f16bdbfaa119acd121bb1aa320d534d
Nitish <rananitish37@gmail.com>
2023-05-28 15:41:54 +0530

    .\IndianCoinChange.java using Greedy not optimal

---
+++ Greedy/IndianCoinChange.java
@@ -0,0 +1,32 @@
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class IndianCoinChange {
+    public static void main(String[] args) {
+        int amount=590;
+        int []coins={1,2,5,10,20,50,100,500,2000};
+        int count=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+        Arrays.sort(coins);  
+        for(int i=coins.length-1;i>=0;i--){
+            if(coins[i]<=amount){
+                while(coins[i]<=amount){
+                    count++;
+                    ans.add(coins[i]);
+                    amount=amount-coins[i];
+                }
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            System.out.print(ans.get(i)+" ");
+        }System.out.println();
+        if(count>0 && amount==0){
+            System.out.println(count);
+        }else{
+            System.out.println("-1");
+        }
+    }
+}
