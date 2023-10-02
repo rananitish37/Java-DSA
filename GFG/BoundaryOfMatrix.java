f2ef01edc08c6cb098fff46bdb42c3faac068b4f
Nitish <rananitish37@gmail.com>
2023-10-02 19:31:43 +0530

    .\BooleanMatrix.java

---
+++ GFG/BoundaryOfMatrix.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+class BoundaryOfMatrix
+{
+    public static void main(String args[])throws IOException
+    {
+            int a[][] = {{1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15,16}};
+            //o/p=1 2 3 4 8 12 16 15 14 13 9 5
+            int n =a.length;
+            int m =a[0].length;
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.boundaryTraversal(a, n, m);
+            for (int i : ans) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+}
+class Solution
+{
+    static ArrayList<Integer> boundaryTraversal(int matrix[][], int n, int m)
+    {
+        // code here 
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        if(n==1)
+            for(int i=0;i<m;i++) ans.add(matrix[0][i]);
+        else if(m==1)
+            for(int i=0;i<n;i++) ans.add(matrix[i][0]);
+        else{
+            for(int i=0;i<m-1;i++) ans.add(matrix[0][i]);
+            for(int i=0;i<n-1;i++) ans.add(matrix[i][m-1]);
+            for(int i=m-1;i>0;i--) ans.add(matrix[n-1][i]);
+            for(int i=n-1;i>0;i--) ans.add(matrix[i][0]);
+        }
+        
+        return ans;
+    }
+}
