6464052e4ff0a9c07c968b4cc230eb45555fc7cc
Nitish <rananitish37@gmail.com>
2023-10-02 19:25:59 +0530

    2 oct 2023 POTD NoOfDistSubSeq.java

---
+++ GFG/NoOfDistSubSeq.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class NoOfDistSubSeq {
+    public static void main(String args[]) throws IOException {
+            String s = "gfg";
+            Solution ob = new Solution();
+            System.out.println(ob.distinctSubsequences(s));
+        }
+    }
+class Solution {
+    int mod=(int)1e9+7;
+    
+    int distinctSubsequences(String S) {
+      int n = S.length();
+      long [] dp = new long[n + 1];
+      
+      dp[0] = 1;
+      HashMap<Character, Integer> lo = new HashMap<>();
+      
+      for(int i=1; i < dp.length; i++){
+          dp[i] = (2 * dp[i-1]) % mod;
+          char ch = S.charAt(i-1);
+          
+          if(lo.containsKey(ch)){
+              int j =  lo.get(ch);
+              dp[i] = (dp[i] - dp[j] + mod) % mod;
+          }
+          lo.put(ch, (i-1) % mod);
+          
+      }
+      return (int)(dp[n] % mod);
+    }
+}
