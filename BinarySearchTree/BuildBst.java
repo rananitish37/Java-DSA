500ba6a63270da4ca3321be48ab60351affd65a1
Nitish <rananitish37@gmail.com>
2023-10-02 19:01:56 +0530

    deletion of node in BST BuildBst.java

--- BinarySearchTree/BuildBst.java
+++ BinarySearchTree/BuildBst.java
@@ -28,32 +28,65 @@ public class BuildBst{
         System.out.print(root.data+" ");
         printTree(root.right);
     }
-    public static boolean Search(Node root,int key){
+    public static boolean search(Node root,int key){
         if(root==null){
             return false;
         }
         if(root.data==key){
             return true;
         }else if(root.data>key){
-            return Search(root.left, key);
+            return search(root.left, key);
         }else{
-            return Search(root.right, key);
+            return search(root.right, key);
         }
     }
+    public static Node deleteNode(Node root, int node){
+        if(root.data<node){
+            root.right=deleteNode(root.right, node);
+        }else if(root.data>node){
+            root.left=deleteNode(root.left, node);
+        }else{
+            //case 1 --> leaf node
+            if(root.right==null && root.left == null){
+                return null;
+            }
+            //case 2 --> single child
+            else if(root.right==null){
+                return root.right;
+            }
+            else if(root.left==null){
+                return root.left;
+            }
+            
+            //case 3 --> two child
+            Node IS=findInorderSuccessor(root.right);
+            root.data=IS.data;
+            root.right=deleteNode(root.right, IS.data);
+        }
+        return root;
+    }
+    public static Node findInorderSuccessor(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
-        int val[]={1,2,5,7,3,9};
+        int val[]={8,5,3,1,4,6,10,11,14};
         Node root=null;
 
         for(int i=0;i<val.length;i++){
             root=insert(root, val[i]);
         }
         printTree(root);
-        if(Search(root, 3)){
-            System.out.println("found");
-        }else{
-            System.out.println("Not found");
-        }
-        
-        
+        // if(search(root, 10)){
+        //     System.out.println("found");
+        // }else{
+        //     System.out.println("Not found");
+        // }
+        //DELETE A NODE
+        deleteNode(root, 5);
+        System.out.println();
+        printTree(root);
     }
 }
