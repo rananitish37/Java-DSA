b455d96d4559d71406246bc7e485920dd270b6fb
Nitish <rananitish37@gmail.com>
2024-03-11 19:05:23 +0530

    implemented priority queue using jcf

---
+++ PriorityQueue/PQJCF.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PQJCF {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(5);
+        pq.add(7);
+        pq.add(1);
+        pq.add(3);
+        pq.add(2);
+
+        while (!pq.isEmpty()) {
+            System.out.print(pq.peek()+" ");
+            pq.remove();
+        }
+    }
+}
