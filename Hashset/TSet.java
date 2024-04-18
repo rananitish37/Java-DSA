4ebd6e0abaca8053f3449fe12fd4c32a1e9ef7a6
Nitish <rananitish37@gmail.com>
2024-04-18 17:05:21 +0530

    Now done with TreeSet too

---
+++ Hashset/TSet.java
@@ -0,0 +1,16 @@
+package Hashset;
+
+import java.util.TreeSet;
+
+public class TSet {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        ts.add(1);
+        ts.add(2);
+        ts.add(3);
+        ts.add(4);
+
+        System.out.println(ts);
+    }
+}
